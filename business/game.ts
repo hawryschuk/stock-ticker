183991227136d57e41a49c60d0f863d79cefb465
Alex Hawryschuk <alex@hawryschuk.com>
2022-04-17 17:57:32 -0500

    no message

--- business/game.ts
+++ business/game.ts
@@ -1,22 +1,62 @@
 import { Player } from "./player";
 import { Commodity } from "./commodity";
 import { Util } from "./util";
-// import { table } from 'table';
-import { Table } from 'c:/Users/owner/Documents/@hawryschuk-terminal-restapi/Table';
-import { Terminal } from 'c:/Users/owner/Documents/@hawryschuk-terminal-restapi/Terminal';
+import { Table, Terminal, BaseService, Prompt, TableService } from "../../@hawryschuk-terminal-restapi";
 
+export class AutoTerminal extends Terminal {
+    async prompt(options: Prompt) {
+        return await super.prompt({ ...options, resolved: 'initial' in options ? options.initial : null });
+    }
+}
 
-/** In the game, all a player can do is  */
-export class Game {
+/** Stock-Ticker : spot prices, player assets */
+// https://magisterrex.files.wordpress.com/2014/07/stocktickerrules.pdf
+export class Game extends BaseService {
     table: Table;
     players: Player[];
     turn: Player;
     commodities: { [name: string]: Commodity } = ['oil', 'bonds', 'gold', 'silver', 'grain', 'industrial'].reduce((commodities, name) => ({ ...commodities, [name]: new Commodity(name) }), {});
     canTrade = true;    // Once the user rolls the dice, they cannot trade
-    constructor({ table } = {} as { table: Table }) {
-        this.table = table;
-        this.players = table.terminals.map(terminal => new Player(terminal, this));
+    history: {
+        amount: number;
+        direction: 'up' | 'down' | 'dividend';
+        commodity: Commodity
+    }[] = [];
+    rolled = {
+        amount: 0,
+        direction: undefined as 'up' | 'down' | 'dividend',
+        commodity: undefined as Commodity
+    };
+
+    constructor({ table = null, terminals = [] } = {} as { table?: Table; terminals?: Terminal[]; }) {
+        super({ table });
+        this.players = terminals.map((terminal, i) => new Player(terminal || new AutoTerminal({ history: [{ type: 'prompt', options: { name: 'name', type: 'text', message: '', resolved: `Robot ${i + 1}` } }] }), this));
         this.turn = this.players[0];
+        if (this.serviceState) this.state = this.serviceState;
+    }
+
+    static FINISHED_WHEN_HISTORY = 100;
+
+    get finished() { return this.history.length > Game.FINISHED_WHEN_HISTORY }
+
+    get Commodities() { return Object.values(this.commodities) }
+
+    get terminal(): Terminal {
+        const players: Player[] = this.players.filter(player => player.terminal && new TableService(player.terminal).serviceState);
+        return players.includes(this.turn) ? this.turn.terminal : players[0]?.terminal;
+    }
+
+    get serviceState() { return this.terminal && new TableService(this.terminal).serviceState; }
+
+    get losers() { return this.players.filter(p => p !== this.winner) }
+
+    get winner() {
+        return this
+            .players
+            .map(player => ({ player, worth: player.worth }))
+            .sort((a, b) => a.worth - b.worth)
+            .pop()
+            .player
     }
 
     /** FEAT: Trade something */
@@ -28,18 +68,6 @@ export class Game {
         }
     }
 
-    history: {
-        amount: number;
-        direction: 'up' | 'down' | 'dividend';
-        commodity: Commodity
-    }[] = [];
-
-    rolled = {
-        amount: 0,
-        direction: undefined as 'up' | 'down' | 'dividend',
-        commodity: undefined as Commodity
-    };
-
     /** FEAT: RollDice */
     roll({
         amount = Util.random([5, 10, 20]),
@@ -50,9 +78,8 @@ export class Game {
         this.canTrade = false;
 
         Object.assign(this.rolled, { amount, direction, commodity });
-        this.history.push({ ...this.rolled });
 
-        console.log({ amount, direction, commodity });
+        this.history.push({ ...this.rolled });
 
         if (/up|down/.test(direction)) {
             commodity.price += amount / 100 * (direction === 'up' ? 1 : -1);
@@ -76,161 +103,142 @@ export class Game {
             /** FEAT: Dividend */
             for (const { assets } of this.players) {
                 const dividend = assets[commodity.name] * amount / 100;
-                assets[commodity.name] += dividend;
+                if (commodity.price >= 1) assets[commodity.name] += dividend;
             }
         }
 
         /** FEAT: Next Player */
-        if (direction === 'down') {
-            this.turn = this.players[this.players.indexOf(this.turn) + 1] || this.players[0];
-            this.canTrade = true;
-        }
+        // if (direction === 'down') {
+        this.turn = this.players[this.players.indexOf(this.turn) + 1] || this.players[0];
+        this.canTrade = true;
+        // }
     }
 
-    /** ASCII Table */
-    toString() {
-        const data = [
-            ['Last Roll', '-Commodity-', '-What-', '-Amount-'],
-            this.rolled.commodity ? ['', this.rolled.commodity.name, this.rolled.direction, this.rolled.amount] : [],
-            ['Turn', this.turn.name],
-            ['Commodities', 'Name', 'Price'],
-            ...Object.values(this.commodities).map(c => ['', c.name, c.price]),
-            ['Players', 'Name', 'Net Worth', 'Cash', ...Object.keys(this.commodities)],
-            ...this.players.map(player => {
-                const { name, worth, cash, assets } = player;
-                return [this.turn === player ? '*' : '', name, worth, cash, ...Object.keys(this.commodities).map(name => assets[name])];
-            })
-        ];
-        const maxColumns = Math.max(...data.map(row => row.length));
-        for (const row of data) {
-            while (row.length < maxColumns) {
-                row.push('');
-            }
-        }
-        if (0) {/** Put history in column maxColumn */
-            for (let i = data.length; i < this.history.length; i++) { data[i] = new Array(maxColumns).fill(''); }
-            for (const row of data) { for (let index = maxColumns; index < maxColumns + 3; index++) { row[index] = '_'; } }
-            for (let i = 0; i < this.history.length; i++) {
-                const { commodity, direction, amount } = this.history.reverse()[i];
-                data[i].splice(maxColumns, 3, commodity.name, direction, amount);
-            }
-        }
-        return table(data);
-    }
-
-    JSON() {
-        return JSON.stringify({
-            'Turn': this.turn.name,
-            'Spot Prices': this.commodities,
-            'Net Worths': this.players.map(player => ({
+    get state() {
+        return {
+            history: this.history,
+            turn: this.turn?.name,
+            commodities: this.commodities,
+            rolled: this.rolled,
+            players: this.players.map(player => ({
                 name: player.name,
                 cash: player.cash,
-                commodities: player.assets,
+                assets: player.assets,
                 worth: player.worth
             }))
-        }, null, 2);
+        };
+    }
+
+    set state({ history, commodities, rolled, players, turn }) {
+        players.forEach(({ name, cash, assets }, i) => {
+            Object.assign(this.players[i], {
+                cash,
+                assets,
+                name
+            });
+        });
+        Object.assign(this, {
+            history,
+            commodities,
+            rolled,
+            turn: this.players.find(p => p.name === turn),
+        });
+        players.forEach(({ name, cash, assets }, i) => {
+            Object.assign(this.players[i], {
+                cash,
+                assets,
+                name
+            });
+        });
+        // debugger;
     }
 
     /** Play the game through prompts and stdout */
-    static async play({
-        numPlayers = undefined as number,
-        stdout = console.log as Function,
-        prompt = undefined as (options: Prompt | Prompt[]) => any,
-    }) {
-        numPlayers = numPlayers || parseInt((await prompt({
-            type: 'number',
-            name: 'numPlayers',
-            message: 'How many players?',
-            min: 2,
-            initial: 2,
-        })).numPlayers);
-        const players: string[] = await prompt(new Array(numPlayers).fill(0).map((_, index) => {
-            return {
-                type: 'text',
-                name: `${index}`,
-                message: `What is Player #${index + 1}'s name?`
-            };
-        }))
-            .then((players: any) => new Array(numPlayers).fill(0).map((_, index) => {
-                return players[index]
-            }));
+    async auto(game = this) {
+        if (!this.turn) throw new Error('Nobodys turn!');
+        if (game.finished) throw new Error('Game Finished: Nothing to auto');
+        if (!game.serviceState) { await game.broadcast(JSON.stringify({ state: this.state })); } // broadcast the game state for the first time 
 
-        const game = new Game(players);
-        while (1) {
-            stdout(game.toString());
-            let amount = 1, makeTrade = true;
-            while (makeTrade && amount && game.canTrade) {/** Make a trade */
-                stdout(game.toString());
-                makeTrade = (await prompt({
-                    type: 'confirm',
-                    name: 'makeTrade',
-                    message: `${game.turn.name}, it is your turn. Would you like to buy/sell any commodities?`,
-                    initial: true
-                })).makeTrade;
-                if (makeTrade) {
-                    let sell = false;
-                    let { commodity } = await prompt({
-                        type: 'select',
-                        name: 'commodity',
-                        message: 'Which commodity would you like to buy/sell',
-                        choices: Object.values(game.commodities).map(commodity => {
-                            const { name, price } = commodity;
-                            const shares = game.turn.assets[name];
-                            return {
-                                value: commodity,
-                                title: name,
-                                description: `Market Price: ${price}.
-                                                You own @ ${shares} shares
-                                                -- a market value of (${price * shares}$).
-                                        `,
-                            };
-                        })
+        //#region get choice from Choices[ title,value, handler ]
+        const choices = [
+            {
+                title: 'roll dice',
+                value: 'roll-dice',
+            },
+            ...Object.entries(game.turn?.assets || {}).filter(([name, units]) => units).map(([name, units]) => ({
+                value: `sell-${name}`,
+                title: `Sell ${name} (${units}-units @ ${this.commodities[name].price} => ${units * this.commodities[name].price})`
+            })),
+            ...Object.entries(game.commodities).filter(() => game.turn?.cash).map(([name, commodity]) => ({
+                value: `buy-${name}`,
+                title: `Buy ${name} (${Math.floor(game.turn.cash / commodity.price)}-units @ $${commodity.price})`,
+                disabled: !game.turn.cash
+            })),
+        ];
+        const handlers = {
+            'roll-dice': async () => {
+                const { name } = this.turn;
+                game.roll();
+                await game.broadcast(`${name} rolled ${JSON.stringify(game.rolled)}`);
+                if (game.finished) {
+                    await game.broadcast(`${this.winner.name} wins the game`);
+                    console.log('finished game', game.players.map(p => p.terminal));
+                    await Promise.all(game.players.map(p => p.terminal.prompt({ name: 'ack_game', message: 'Game over, press any key to continue', type: 'text' })))
+                    this.results = { winners: [this.winner.name], losers: this.losers.map(l => l.name) };
+                }
+            },
+            ...Object.entries(game.turn?.assets || {}).filter(() => this.canTrade).reduce((handlers, [name, units]) => ({
+                ...handlers,
+                [`sell-${name}`]: async () => {
+                    const _units = await game.turn.terminal.prompt({
+                        message: `How many units of ${name} would you like to sell? (max=${units})`,
+                        name: 'units',
+                        type: 'number',
+                        min: 0,
+                        max: units,
+                        initial: units,
                     });
-                    const owns = game.turn.assets[commodity.name];
-                    if (owns) {
-                        let { sell } = await prompt({
-                            type: 'confirm',
-                            name: 'sell',
-                            message: `Would you like to sell any of your ${owns} shares?`,
-                        });
-                        if (sell) {
-                            amount = await prompt({
-                                type: 'number',
-                                name: 'amount',
-                                initial: 0,
-                                max: owns,
-                                min: 0,
-                                message: `How many of your ${owns} shares would you like to sell?`,
-                            }).then(_ => _.amount);
-                            if (amount) {
-                                game.turn.trade('sell', commodity, amount);
-                            }
-                        }
-                    }
-
-                    if (!sell) {
-                        amount = await prompt({
-                            type: 'number',
-                            name: 'amount',
-                            initial: 0,
-                            max: Math.floor(game.turn.cash / commodity.price),
-                            min: 0,
-                            message: `How many shares would you like to buy?`,
-                        }).then(_ => _.amount);
-                        if (amount) {
-                            game.turn.trade('buy', commodity, amount);
-                        }
+                    if (_units > 0 && _units <= units) {
+                        game.trade('sell', game.commodities[name], units);
+                        await game.broadcast(`${game.turn.name} has sold ${units} of ${name} @ ${game.commodities[name].price} per share for $ $${units * game.commodities[name].price}`);
                     }
                 }
-            }
-            {/** Roll the dice */
-                await prompt({
-                    type: 'text',
-                    name: 'any',
-                    message: `Press any key to roll the dice`,
-                });
-                game.roll();
-            }
-        }
+            }), {}),
+            ...Object.entries(game.commodities).filter(() => this.canTrade).reduce((handlers, [name, commodity]) => ({
+                ...handlers,
+                [`buy-${name}`]: async () => {
+                    const units = await game.turn.terminal.prompt({
+                        message: `How many units of ${name} would you like to buy? (max=${Math.floor(game.turn.cash / commodity.price)})`,
+                        name: 'units',
+                        type: 'number',
+                        min: 0,
+                        max: Math.floor(game.turn.cash / commodity.price),
+                        initial: Math.floor(game.turn.cash / commodity.price),
+                    });
+                    game.trade('buy', commodity, units);
+                    await game.broadcast(`${game.turn.name} has bought ${units} of ${name} @ ${commodity.price} per share for $ $${units * commodity.price}`);
+                }
+            }), {}),
+        };
+
+        const choice = await this.turn.terminal.prompt({
+            type: 'select',
+            name: 'stock-ticker-action',
+            message: 'what would you like to do?',
+            initial: 'roll-dice',
+            clobber: true,
+            choices
+        });
+        //#endregion
+
+        /** Perform the chosen action */
+        if (!handlers[choice]) console.log('no handler for choice', choice);
+        else if (!(choices as any).find(c => c.value === choice)) console.log('unknown choice', choice)
+        else if ((choices as any).find(c => c.value === choice).disabled) console.log('choice is disabled', choice)
+        else await handlers[choice]();
+
+        /** Broadcast the game state */
+        await game.broadcast(JSON.stringify({ state: this.state }));
     }
 }
+
